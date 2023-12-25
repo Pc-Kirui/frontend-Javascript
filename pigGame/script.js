b0e6e04c86d70cb12f3941c1480b7a8739e96712
Pc-Kirui <patokirui@gmail.com>
2023-12-25 15:21:18 +0300

    implimented reset functionality

--- pigGame/script.js
+++ pigGame/script.js
@@ -13,15 +13,37 @@ const btnNew = document.querySelector(".btn-new");
 const btnRoll = document.querySelector(".btn-roll");
 const btnHold = document.querySelector(".btn-hold");
 
+let scores, currentScore, activePlayer, playing;
+
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+scores = [0, 0];
+currentScore = 0;
+activePlayer = 0;
+playing = true;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  // Resetting initial conditions
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  // Remove the winner class
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player-winner");
+  player1El.classList.remove("player-winner");
+  player1El.classList.add("player-active");
+  player1El.classList.remove("player-active");
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current-${activePlayer}`).textContent = 0;
@@ -79,3 +101,6 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+// Resetting the game
+btnNew.addEventListener("click", init);
